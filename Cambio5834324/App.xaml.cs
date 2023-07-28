0d4048e4b6c98927983d95683a43e8b2ff1e8989
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-07-27 19:29:37 -0600

    Agregar archivos de proyecto.

---
+++ Cambio5834324/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cambio5834324;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Cambio5834324/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Cambio5834324;
+
+public partial class MainPage : ContentPage
+{
+    /// <summary>
+    /// <Createddate>27/07/2023</Createddate>
+    /// <company>INDEL</company>
+    /// <lastmodificationdate>27/07/2023</lastmodificationdate>
+    /// <lastmodificationdescription>
+    /// PROPIEDADES DE LA IMAGEN
+    /// </lastmodificationdescription>
+    /// <lastmodifierautor>ELISEO</lastmodifierautor>
+    /// </summary>
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    /// <summary>
+    /// SE EJECUTA AL PRESIONAR EL BOTON
+    /// </summary>
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        double costo = Convert.ToDouble(costoArt.Text);
+        double dinero = Convert.ToDouble(dinerocli.Text);
+
+        double cambio = dinero - costo;
+
+        cambioCli.Text = "EL cambio es: $" + cambio;
+    }
+}
+
